fee7662fd38e892b53d8ba5bb7622e13886f09c6
SeokHyeon-Hwang <42320146+SeokHyeon-Hwang@users.noreply.github.com>
2018-12-07 13:49:55 +0900

    181207 period execute function

---
+++ 181207_period_execute_function.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  7 10:53:54 2018
+
+@author: ktm
+"""
+
+# 주기적으로 실행되는 함수 만들기
+#%%
+#
+import threading
+
+class AsyncTask:
+    def __init__(self):
+        pass
+    
+    def TaskA(self):
+        print 'Process A'
+        threading.Timer(1, self.TaskA).start()
+        
+    def Task(self):
+        print 'Process B'
+        threading.Timer(3, self.TaskB).start()
+        
+def main():
+    print 'Async Function'
+    at = AsyncTask()
+    at.TaskA()
+    at.TaskB()
+    
+if __name__ == '__main__':
+    main()
